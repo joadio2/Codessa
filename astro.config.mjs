75119bb4cc6b0cb75898cd29d247c06a27a1c52b
joadio2 <joaquin.dionicioo@gmail.com>
2025-06-25 11:12:15 +0100

    ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: "https://joadio2.github.io/",
+  base: "/Odessa",
+
+  integrations: [react()],
 });
